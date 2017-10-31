98afac7f28b3180f0585b9eabf55dc6e6f86c7b9
Rasmus Dupont Søgaard <rasmusduponts@gmail.com>
2017-10-31 15:37:55 +0100

    SearchRepository added

---
+++ Repositories/SearchRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DataTransferObjects;
+using WebAPI.Entities;
+using WebAPI.Interfaces;
+
+namespace WebAPI.Repositories
+{
+    public class SearchRepository : ISearchRepository
+    {
+
+        public List<Post> GetPostsBySearchString(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO er en procedure
+        public void SaveSearch(SearchDTO search)
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO er en procedure
+        public List<Search> GetSearchHistory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
