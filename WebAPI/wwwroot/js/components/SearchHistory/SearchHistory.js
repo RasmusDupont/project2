182ef669a25b23cd1d3dd9ab7bbc8d1c040389a3
kasper995 <kasper995@hotmail.dk>
2017-12-11 18:38:40 +0100

    foundation for search history made

---
+++ WebAPI/wwwroot/js/components/SearchHistory/SearchHistory.js
@@ -0,0 +1,68 @@
+﻿define (['knockout','dataservice'], function(ko, ds){
+
+    return function (params) {
+
+        var title = ko.observable();
+        var qaPosts = ko.observableArray();
+        var clickMark = function(data, event){
+
+            if (data.markedPost() === true)
+            {
+                data.markedPost(false);
+            }
+            else 
+            {
+                data.markedPost(true);
+            }
+            ds.putMark(data.id,function(d){});
+       
+        }
+        var saveAnnotation = function(data, event){
+
+            ds.putAnnotation(data.id, data.annotation(), function(d){});
+
+        }
+
+        // get posts
+        ds.getPost(427217,
+            function(data) {
+
+ 
+                for(i = 0; i < data.length; i++)
+                {
+                    if(data[0].acceptedAnswerId === data[i].id)
+                    {
+                        data[i].acceptedAnswer = ko.observable(true);
+                    }
+                    else
+                    {
+                        data[i].acceptedAnswer = ko.observable(false);
+                    }
+                }
+
+
+                //make markedPost observable
+                for(i = 0;i < data.length;i++){
+
+                    data[i].markedPost = ko.observable(data[i].markedPost);
+                    data[i].annotation = ko.observable(data[i].annotation);
+
+                }
+
+                console.log(data);
+
+                qaPosts(data);
+                title(data[0].title);
+
+
+            });
+
+        return {
+            title,
+            qaPosts,
+            clickMark,
+            saveAnnotation
+        };
+
+    }
+});
