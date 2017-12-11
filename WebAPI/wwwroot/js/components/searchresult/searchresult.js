24935dae121c1b23200f4940e5ea06e0275f97c8
Kristian Kronstrand <k.kronstrand@gmail.com>
2017-12-12 00:53:26 +0100

    added paging

--- WebAPI/wwwroot/js/components/searchresult/searchresult.js
+++ WebAPI/wwwroot/js/components/searchresult/searchresult.js
@@ -2,25 +2,61 @@ define (['knockout','dataservice'], function(ko, ds){
     
         return function (params) {
 
+            var searchString;
+            var page;
+            var pageSize;
             var result = ko.observableArray();
             var next = ko.observable();
             var prev = ko.observable();
             var choosePost = function(data, event){
                 alert("data.id");
             }
+            var prevPage = function(data,evnt){
+                loadViewModel(searchString, page-1, pageSize);
+            }
+            var nextPage = function(data, event){
+                loadViewModel(searchString, page+1, pageSize);
+            }
+            var loadViewModel = function(search, p, pSize){
 
+                ds.searchPosts(search, p, pSize, function(data){
+                    
+                    if(data.prevLink === null){
+                        prev(false);
+                    }
+                    else{
+                        prev(data.prevLink);
+                    }
+    
+                    if(data.nextLink === null){
+                        next(false);
+                    }
+                    else{
+                        next(data.prevLink);
+                    }
+    
+                    result(data.content);
+
+                    page = p;
+                    pageSize = pSize;
+                    searchString = search;
+
+                    next(data.nextLink);
+                    prev(data.prevLink);
+                    console.log(data);
+                })
+
+            }
             
-            ds.searchPosts("sql database javascript", 1, 10, function(data){
-                
-                result(data.content);
-                next(data.nextLink);
-                prev(data.prevLink);
-                console.log(data);
-            })
+            loadViewModel("sql", 0, 10);
 
             return {
                 result,
-                choosePost
+                choosePost,
+                prev,
+                next,
+                prevPage,
+                nextPage
             };
 
         }
