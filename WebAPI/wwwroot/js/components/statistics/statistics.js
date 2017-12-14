96769c48c77ab00d9cd3249806ef8743eb6d3af9
Rasmus Dupont Søgaard <rasmusduponts@gmail.com>
2017-12-14 10:13:31 +0100

    both graphs are implemented

--- WebAPI/wwwroot/js/components/statistics/statistics.js
+++ WebAPI/wwwroot/js/components/statistics/statistics.js
@@ -1,16 +1,13 @@
 ﻿define(['knockout', 'dataservice'], function (ko, ds) {
 
-    return function (params) {
+    return function(params) {
 
         var title = ko.observableArray();
 
-        /**
-         * MOST VIEWED POSTS IS USED AS A TEMPORARY EXAMPLE *
-         */
         var graphObjects = [];
+        var graphObjectsTwo = [];
 
         ds.getMostViewedPosts(15, function(data) {
-
             for (var i = 0; i < data.length; i++) {
                 var array = [];
                 if (data[i]['title'] != null) {
@@ -20,15 +17,24 @@
                 }
                 graphObjects.push(array);
             }
+            console.table(graphObjects);
+
+            ds.getMostUsedTags(15, function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    var array = [data[i]['name'], data[i]['searchCount']];
+                    graphObjectsTwo.push(array);
+                }
+                console.table(graphObjectsTwo);
+            }); 
         });
 
         google.charts.load('current', { packages: ['corechart', 'bar'] });
         google.charts.setOnLoadCallback(drawMostViewedPosts);
+        google.charts.setOnLoadCallback(drawMostUsedTags);
 
         function drawMostViewedPosts() {
             graphObjects.unshift(['Post','Views']);
             var data = google.visualization.arrayToDataTable(graphObjects);
-
             var options = {
                 title: 'Most viewed posts',
                 chartArea: { width: '50%' },
@@ -40,12 +46,27 @@
                     title: 'Post'
                 }
             };
-
-            var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-
+            var chart = new google.visualization.BarChart(document.getElementById('postGraph'));
             chart.draw(data, options);
         }
 
+        function drawMostUsedTags() {
+            graphObjectsTwo.unshift(['Tag', 'Frequency']);
+            var data = google.visualization.arrayToDataTable(graphObjectsTwo);
+            var options = {
+                title: 'Most used tags',
+                chartArea: { width: '50%' },
+                hAxis: {
+                    title: 'Frequency',
+                    minValue: 0
+                },
+                vAxis: {
+                    title: 'Tag'
+                }
+            };
+            var chart = new google.visualization.BarChart(document.getElementById('tagGraph'));
+            chart.draw(data, options);
+        }
 
         return {
             title
