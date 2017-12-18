de7de9264e323f82c6859111307652187a888c63
Rasmus Dupont Søgaard <rasmusduponts@gmail.com>
2017-12-18 17:33:02 +0100

    d3js library added + graph code in termnetwork component

--- WebAPI/wwwroot/js/components/termnetwork/termnetwork.js
+++ WebAPI/wwwroot/js/components/termnetwork/termnetwork.js
@@ -5,56 +5,57 @@
             ko.observableArray();
 
             ds.getTermNetwork("mongodb", function (data) {
-                //var width = 960,
-                //    height = 500;
+                var graph = data["responseText"];
 
-                //var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+                var width = 960,
+                    height = 500;
 
-                //var force = d3.layout.force()
-                //    .gravity(0.05)
-                //    .distance(100)
-                //    .charge(-200)
-                //    .size([width, height]);
+                var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
 
-                //var color = d3.scale.category20c();
+                var force = d3.layout.force()
+                    .gravity(0.05)
+                    .distance(100)
+                    .charge(-200)
+                    .size([width, height]);
 
-                //(function () {
-                //    force
-                //        .nodes(graph.nodes)
-                //        .links(graph.links)
-                //        .start();
+                var color = d3.scale.category20c();
 
-                //    var link = svg.selectAll(".link")
-                //        .data(graph.links)
-                //        .enter().append("line")
-                //        .attr("class", "link")
-                //        .attr("stroke-width", function (d) { return Math.sqrt(d.value); });
+                (function () {
+                    force
+                        .nodes(graph.nodes)
+                        .links(graph.links)
+                        .start();
 
-                //    var node = svg.selectAll(".node")
-                //        .data(graph.nodes)
-                //        .enter().append("g")
-                //        .attr("class", "node")
-                //        .call(force.drag);
-                //    node.append("circle")
-                //        .attr("r", function (d) { return 5; })
-                //        .style("fill", "red")
+                    var link = svg.selectAll(".link")
+                        .data(graph.links)
+                        .enter().append("line")
+                        .attr("class", "link")
+                        .attr("stroke-width", function (d) { return Math.sqrt(d.value); });
 
-                //    node.append("text")
-                //        .attr("dx", function (d) { return -(d.name.length * 3) })
-                //        .attr("dy", ".65em")
-                //        .text(function (d) { return d.name });
+                    var node = svg.selectAll(".node")
+                        .data(graph.nodes)
+                        .enter().append("g")
+                        .attr("class", "node")
+                        .call(force.drag);
+                    node.append("circle")
+                        .attr("r", function (d) { return 5; })
+                        .style("fill", "red")
 
-                //    force.on("tick", function () {
-                //        link.attr("x1", function (d) { return d.source.x; })
-                //            .attr("y1", function (d) { return d.source.y; })
-                //            .attr("x2", function (d) { return d.target.x; })
-                //            .attr("y2", function (d) { return d.target.y; });
-                //        node.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
-                //    });
-                //})();
+                    node.append("text")
+                        .attr("dx", function (d) { return -(d.name.length * 3) })
+                        .attr("dy", ".65em")
+                        .text(function (d) { return d.name });
 
-                alert("help me!");
-                console.log(data);
+                    force.on("tick", function () {
+                        link.attr("x1", function (d) { return d.source.x; })
+                            .attr("y1", function (d) { return d.source.y; })
+                            .attr("x2", function (d) { return d.target.x; })
+                            .attr("y2", function (d) { return d.target.y; });
+                        node.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
+                    });
+                })();
+
+                console.log(data["responseText"]);
                 return data;
             });
         }
